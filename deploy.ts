bd5976fabf4fbb227c722f54a7a34277b98669f3
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-07 15:25:40 +0530

    Move RPC_URL to an envvar as well
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- deploy.ts
+++ deploy.ts
@@ -1,52 +1,73 @@
-import { ethers, JsonRpcProvider } from "ethers";
+import {
+  ethers,
+  JsonRpcProvider,
+  Wallet,
+  ContractFactory,
+  BaseContract,
+} from "ethers";
 import { Provider } from "ganache";
 import * as fs from "node:fs";
 // import required modules
 
 async function main(): Promise<void> {
-  // Get this from Ganache
-  // RPC URL: http://127.0.0.1:8545
-  const provider = new JsonRpcProvider("http://127.0.0.1:8545");
-  // Create a wallet instance
-  // We need the private key to sign transactions
-  // Get it from Ganache
-  // DO NOT REVEAL YOUR PRIVATE KEY OR COMMIT IT TO A PUBLIC REPO
-  // Set it in an envrc which only you have access to `chmod 600 .envrc`
-  const wallet = new ethers.Wallet(process.env.GANACHE_PRIVATE_KEY!, provider);
-  // These ^^ two lines are needed to connect to Blockchain
+  /*
+  / We need two things to make a basic connection to the blockchain
+  / RPC_URL: The URL of the blockchain node we want to connect to, like "http://127.0.0.1:8545"
+  / PRIVATE_KEY: The private key of the account we want to use to deploy the contract
+  / We can get both of these from Ganache or Hardhat
+  / For security reasons, we should not hardcode the private key in the code
+  / Set RPC_URL and PRIVATE_KEY in an envrc which only you have access to (`chmod 600 .envrc`)
+  / Reload the shell before typing `ts-node deploy.ts` by `source .envrc`
+  */
 
-  // To access our contract, or rather contract's ABI and Binary
-  // We need to access the filesystem so we need the fs-extra module
-  // We should read the files synchronusly
-  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
-  const binary = fs.readFileSync(
+  // check if the environment variables are set
+  if (!process.env.RPC_URL || !process.env.PRIVATE_KEY) {
+    throw new Error("Missing environment variables: RPC_URL or PRIVATE_KEY");
+  }
+
+  // DOCS: https://docs.ethers.org/v6/api/providers/jsonrpc/#about-jsonrpcProvider
+  const provider: JsonRpcProvider = new JsonRpcProvider(process.env.RPC_URL!);
+  // Create a wallet instance
+  const wallet: Wallet = new Wallet(process.env.PRIVATE_KEY!, provider);
+
+  // To access our contract, we need to read the contract's
+  // ABI, and Binary files synchronously
+  const abi: string = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.abi",
+    "utf8",
+  );
+  const binary: string = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8",
   );
 
-  // In Ethers contractFactory is an object to deploy the contract
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  console.log("Deploying, please wait...");
+  // In Ethers contractFactory is a class to deploy the contract
+  const contractFactory: ContractFactory = new ContractFactory(
+    abi,
+    binary,
+    wallet,
+  );
+
   // Wait for contract to be deployed
-  const contract = await contractFactory.deploy();
+  console.log("Deploying, please wait...");
+  const contract: BaseContract = await contractFactory.deploy();
   await contract.waitForDeployment();
 
   // call our retrieve() function in the SimpleStorage contract
   // DOCS: https://docs.ethers.org/v6/api/contract/#BaseContract-getFunction
-  let currentFavoriteNumber = await contract.getFunction("retrieve")();
+  let currentFavoriteNumber: number = await contract.getFunction("retrieve")();
   console.log(`Current favorite number: ${currentFavoriteNumber.toString()}`);
 }
 
 // Since main function is an async function
-// we need then, catch
+// which needs `then`, `catch` when calling
 main()
-  // on successful execution we exit the process with 0
   .then(() => {
+    // on successful execution we exit the process with 0
     console.log("Deployment completed");
     process.exit(0);
   })
-  // on error we exit the process with 1
-  .catch((error) => {
-    console.error(error);
+  .catch((error: unknown) => {
+    console.error("Error:", error);
     process.exit(1);
   });
